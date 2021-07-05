10e2aef76adba043b6c561d33f9192099f933c97
WonJW <85880605+WonJW@users.noreply.github.com>
2021-07-05 21:01:08 +0900

    bug fix and requried

--- game.js
+++ game.js
@@ -1,20 +1,26 @@
 const submitButtom = document.querySelector(".guess-num__btn")
 const result = document.querySelector(".result")
+const rangeNum = document.querySelector(".range-num__input")
+const gueesNum = document.querySelector(".guess-num__input")
+const resultGuess = document.querySelector(".result__guess")
+const resultFinal = document.querySelector(".result__final")
 
 function playGame() {
-    const rangeNum = document.querySelector(".range-num__input")
-    const gueesNum = document.querySelector(".guess-num__input")
-    const resultGuess = document.querySelector(".result__guess")
-    const resultFinal = document.querySelector(".result__final")
-    const computerChoice = Math.floor(Math.random() * rangeNum.value)
-    result.classList.remove("hidden")
-    resultGuess.innerText = `Your choice is ${gueesNum.value} and computer choice is ${computerChoice}.`
-    if(parseInt(gueesNum.value) === computerChoice){
-        resultFinal.innerText = "You win!"
-    } else {
-        resultFinal.innerText = "You lose..."
+    if (rangeNum.value === "" || gueesNum.value === ""){
+        result.classList.remove("hidden")
+        resultFinal.classList.add("hidden")
+        resultGuess.innerText = "Please wirte a all number!"
+    } else {       
+        const computerChoice = Math.floor(Math.random() * rangeNum.value)
+        result.classList.remove("hidden")
+        resultFinal.classList.remove("hidden")
+        resultGuess.innerText = `Your choice : ${gueesNum.value}, computer choice : ${computerChoice}.`
+        if(parseInt(gueesNum.value) === computerChoice){
+            resultFinal.innerText = "You win!"
+        } else {
+            resultFinal.innerText = "You lose..."
+        }
     }
-
 };
 
 submitButtom.addEventListener("click", playGame)
