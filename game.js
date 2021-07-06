27c5e4e56278ee3e76271c03de7643c66ce23d4b
WonJW <85880605+WonJW@users.noreply.github.com>
2021-07-07 01:20:03 +0900

    used input number required

--- game.js
+++ game.js
@@ -1,34 +1,25 @@
 const range = document.querySelector(".range-num__input")
 const guess = document.querySelector(".guess-num__input")
-const submitButtom = document.querySelector(".guess-num__btn")
+const submitButtom = document.querySelector(".numbers")
 const result = document.querySelector(".result")
 const resultGuess = document.querySelector(".result__guess")
 const resultFinal = document.querySelector(".result__final")
 
 
-function playGame() {
+function playGame(event) {
+    event.preventDefault();
     const rangeNum = range.value
     const guessNum = guess.value
-    if (rangeNum === "" || guessNum === "") {
+    if (parseInt(rangeNum) < parseInt(guessNum)) {
         result.classList.remove("hidden")
         resultFinal.classList.add("hidden")
-        if (rangeNum === "" && guessNum === "") {
-            resultGuess.innerText = "Please wirte a all number!"
-        } else if (rangeNum === ""){
-            resultGuess.innerText = "Please wirte a range number!"
-        } else {
-            resultGuess.innerText = "Please wirte a guess number!"
-        }  
-    } else if (parseInt(rangeNum) < parseInt(guessNum)) {
-        result.classList.remove("hidden")
-        resultFinal.classList.add("hidden")
-        resultGuess.innerText = "Your guess number is bigger than range number!"
+        resultGuess.innerText = "Your chosse number is bigger than range number!"
     } else {       
         const computerChoice = Math.floor(Math.random() * rangeNum)
         result.classList.remove("hidden")
         resultFinal.classList.remove("hidden")
         resultGuess.innerText = `Your choice : ${guessNum}, computer choice : ${computerChoice}.`
-        if(parseInt(guessNum) === computerChoice){
+        if(parseInt(guessNum, 10) === computerChoice){
             resultFinal.innerText = "You win!"
         } else {
             resultFinal.innerText = "You lose..."
@@ -36,4 +27,6 @@ function playGame() {
     }
 };
 
-submitButtom.addEventListener("click", playGame)
+if (range.value != null && guess.value != null){
+    submitButtom.addEventListener("submit", playGame);
+}
