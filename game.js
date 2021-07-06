48c55da8024c1c69a1e76126aefc4d759c8fe002
WonJW <85880605+WonJW@users.noreply.github.com>
2021-07-07 01:59:19 +0900

    make valuable of "hidden"

--- game.js
+++ game.js
@@ -4,6 +4,7 @@ const submitButtom = document.querySelector(".numbers")
 const result = document.querySelector(".result")
 const resultGuess = document.querySelector(".result__guess")
 const resultFinal = document.querySelector(".result__final")
+const HIDDEN_CLASSNAME = "hidden"
 
 
 function playGame(event) {
@@ -11,13 +12,13 @@ function playGame(event) {
     const rangeNum = range.value
     const guessNum = guess.value
     if (parseInt(rangeNum) < parseInt(guessNum)) {
-        result.classList.remove("hidden")
-        resultFinal.classList.add("hidden")
-        resultGuess.innerText = "Your chosse number is bigger than range number!"
+        result.classList.remove(HIDDEN_CLASSNAME)
+        resultFinal.classList.add(HIDDEN_CLASSNAME)
+        resultGuess.innerText = "Your guess number is bigger than range number!"
     } else {       
         const computerChoice = Math.floor(Math.random() * rangeNum)
-        result.classList.remove("hidden")
-        resultFinal.classList.remove("hidden")
+        result.classList.remove(HIDDEN_CLASSNAME)
+        resultFinal.classList.remove(HIDDEN_CLASSNAME)
         resultGuess.innerText = `Your choice : ${guessNum}, computer choice : ${computerChoice}.`
         if(parseInt(guessNum, 10) === computerChoice){
             resultFinal.innerText = "You win!"
